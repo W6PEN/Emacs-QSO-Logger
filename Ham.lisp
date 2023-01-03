191d0f21623debc3385c0027a276d45f6cc67f44
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-03 14:32:25 -0800

    Create Ham.lisp

---
+++ Ham.lisp
@@ -0,0 +1,49 @@
+;; Define a function to add a new entry to the logbook
+(defun add-log-entry ()
+  "Prompt the user for information about a new amateur radio contact and add it to the logbook."
+  (interactive)
+  (let ((callsign (read-string "Callsign: "))
+        (mode (read-string "Mode: "))
+        (frequency (read-string "Frequency: "))
+        (date (read-string "Date (YYYY-MM-DD): "))
+        (time (read-string "Time (HH:MM): "))
+        (rst (read-string "RST: "))
+        (operator (read-string "Operator: "))
+        (location (read-string "Location: ")))
+    (with-temp-buffer
+      (insert (format "Callsign: %s\nMode: %s\nFrequency: %s\nDate: %s\nTime: %s\nRST: %s\nOperator: %s\nLocation: %s\n"
+                      callsign mode frequency date time rst operator location))
+      (append-to-file (point-min) (point-max) "~/radio-log.txt"))))
+
+;; Define a function to export the logbook to ADIF format
+(defun export-logbook-adif ()
+  "Export the amateur radio logbook to a file in ADIF format."
+  (interactive)
+  (with-temp-buffer
+    (insert-file-contents "~/radio-log.txt")
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let ((callsign (progn (search-forward "Callsign: ") (thing-at-point 'word)))
+            (mode (progn (search-forward "Mode: ") (thing-at-point 'word)))
+            (frequency (progn (search-forward "Frequency: ") (thing-at-point 'word)))
+            (date (progn (search-forward "Date: ") (thing-at-point 'word)))
+            (time (progn (search-forward "Time: ") (thing-at-point 'word)))
+            (rst (progn (search-forward "RST: ") (thing-at-point 'word)))
+            (operator (progn (search-forward "Operator: ") (thing-at-point 'word)))
+            (location (progn (search-forward "Location: ") (thing-at-point 'word))))
+        (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s\n"
+                        (length callsign) callsign
+                        (length mode) mode
+                        (length frequency) frequency
+                        (length date) date
+                        (length time) time
+                        (length rst) rst
+                        (length operator) operator
+                        (length location) location))
+        (forward-line))
+(write-file "~/radio-logbook.adi")))
+
+
+;; This code defines two functions: `add-log-entry` and `export-logbook-adif`. The `add-log-entry` function prompts the user for information about a new amateur radio contact and adds it to a text file called `radio-log.txt` in the user's home directory. The `export-logbook-adif` function reads the contents of `radio-log.txt` and converts it to ADIF format, writing the result to a file called `radio-logbook.adi`.
+
+;; To use these functions, you would first invoke `add-log-entry` to add one or more entries to the logbook, and then invoke `export-logbook-adif` to export the logbook to ADIF format. You can then use the resulting ADIF file to import the logbook into another logging program or platform that supports the ADIF format.
