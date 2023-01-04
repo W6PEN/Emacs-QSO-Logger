6e62f05e995cad49aa94eaec80e81e2a5d7b0ac2
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-04 15:07:23 -0800

    Update QSO.el

--- QSO.el
+++ QSO.el
@@ -1,13 +1,13 @@
-;; This code defines the function `add-qso` function which prompts the user for information about a new amateur radio contact and adds it to a text file called `radio-log.txt` in the user's home directory and to an ADIF file `radio-logbook.adi`.
+;; This code defines the function `add-qso` function which prompts the user for information about a new amateur radio contact (QSO) and adds it to a text file called `qso-log.txt` and an ADIF file `qso-log.adi` in the user's home directory.
 
-;; To use this functions, you would first invoke `add-qso` to add one or more entries to the logbook, and then import the resulting ADIF file to import the logbook into another logging program or platform that supports the ADIF format for further processing.
+;; To use this functions, you would first invoke `add-qso` to add one or more entries to the logbook, and then process within Emacs or import the ADIF file into another logging program or platform that supports the ADIF format for further processing.
 
 (defun add-qso ()
   "Prompt the user for information about a new amateur radio contact and add it to the logbook."
-  (let ((date (format-time-string "%Y%m%d" (current-time) t))
-        (time (format-time-string "%H%M%S" (current-time) t)))
   (interactive)
   (let ((callsign (read-string "Callsign: "))
+        (date (format-time-string "%Y%m%d" (current-time) t))
+        (time (format-time-string "%H%M%S" (current-time) t))
         (mode (read-string "Mode: "))
         (frequency (read-string "Frequency: "))
         (rstrcvd (read-string "RST Rcvd: "))
@@ -16,19 +16,19 @@
         (location (read-string "Location: "))
         (comment (read-string "Comment: ")))
     (with-temp-buffer
-      (insert (format "Callsign: %s\nMode: %s\nFrequency: %s\nDate: %s\nTime: %s\nRST Rcvd: %s\nRST Sent: %s\nOperator: %s\nLocation: %s\nComment: %s\n"
-                      callsign mode frequency date time rstrcvd rstsent operator location comment))
-      (append-to-file (point-min) (point-max) "~/radio-log.txt")))
+      (insert (format "Callsign: %s\nDate: %s\nTime: %s\nMode: %s\nFrequency: %s\nRST Rcvd: %s\nRST Sent: %s\nOperator: %s\nLocation: %s\nComment: %s\n\n"
+                      callsign date time mode frequency rstrcvd rstsent operator location comment))
+      (append-to-file (point-min) (point-max) "~/qso-log.txt"))
     (with-temp-buffer    
-      (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_RCVD:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s <COMMENT:%d>%s\n"
+      (insert (format "<CALL:%d>%s<QSO_DATE:%d>%s<TIME_ON:%d>%s<MODE:%d>%s<FREQ:%d>%s<RST_RCVD:%d>%s<RST_SENT:%d>%s<NAME:%d>%s<QTH:%d>%s<COMMENT:%d>%s<eor>\n"
                         (length callsign) callsign
-                        (length mode) mode
-                        (length frequency) frequency
                         (length date) date
                         (length time) time
+                        (length mode) mode
+                        (length frequency) frequency
                         (length rstrcvd) rstrcvd
                         (length rstsent) rstsent
                         (length operator) operator
                         (length location) location
                         (length comment) comment))
-      (append-to-file (point-min) (point-max) "~/radio-logbook.adi"))))
+      (append-to-file (point-min) (point-max) "~/qso-log.adi"))))
