547c851f986fb8449b108c3c0469aef6520890f8
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-04 07:14:50 -0800

    Update HamLog.el
    Added RST_RCVD and COMMENT to both functions, removed colon from time, removed hyphens from date.

--- HamLog.el
+++ HamLog.el
@@ -9,14 +9,16 @@
   (let ((callsign (read-string "Callsign: "))
         (mode (read-string "Mode: "))
         (frequency (read-string "Frequency: "))
-        (date (read-string "Date (YYYY-MM-DD): "))
-        (time (read-string "Time (HH:MM): "))
-        (rst (read-string "RST: "))
+        (date (read-string "Date (YYYYMMDD): "))
+        (time (read-string "UTC (HHMM): "))
+        (rstrcvd (read-string "RST Rec'd: "))
+        (rstsent (read-string "RST Sent: "))
         (operator (read-string "Operator: "))
-        (location (read-string "Location: ")))
+        (location (read-string "Location: "))
+        (comment (read-string "Comment: ")))
     (with-temp-buffer
-      (insert (format "Callsign: %s\nMode: %s\nFrequency: %s\nDate: %s\nTime: %s\nRST: %s\nOperator: %s\nLocation: %s\n"
-                      callsign mode frequency date time rst operator location))
+      (insert (format "Callsign: %s\nMode: %s\nFrequency: %s\nDate: %s\nTime: %s\nRST Rec'd: %s\nRST Sent: %s\nOperator: %s\nLocation: %s\nComment: %s\n"
+                      callsign mode frequency date time rstrcvd rstsent operator location))
       (append-to-file (point-min) (point-max) "~/radio-log.txt"))))
 
 ;; Define a function to export the logbook to ADIF format
@@ -30,19 +32,23 @@
       (let ((callsign (progn (search-forward "Callsign: ") (thing-at-point 'word)))
             (mode (progn (search-forward "Mode: ") (thing-at-point 'word)))
             (frequency (progn (search-forward "Frequency: ") (thing-at-point 'word)))
-            (date (progn (search-forward "Date: ") (thing-at-point 'word)))
-            (time (progn (search-forward "Time: ") (thing-at-point 'word)))
-            (rst (progn (search-forward "RST: ") (thing-at-point 'word)))
+            (date (progn (search-forward "Date (YYYYMMDD): ") (thing-at-point 'word)))
+            (time (progn (search-forward "UTC (HHMM): ") (thing-at-point 'word)))
+            (rstrcvd (progn (search-forward "RST Rec'd: ") (thing-at-point 'word)))
+            (rstsent (progn (search-forward "RST Sent: ") (thing-at-point 'word)))
             (operator (progn (search-forward "Operator: ") (thing-at-point 'word)))
-            (location (progn (search-forward "Location: ") (thing-at-point 'word))))
-        (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s\n"
+            (location (progn (search-forward "Location: ") (thing-at-point 'word)))
+            (comment (progn (search-forward "Comment: ") (thing-at-point 'word))))
+        (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_RCVD:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s<COMMENT:%d>%s\n"
                         (length callsign) callsign
                         (length mode) mode
                         (length frequency) frequency
                         (length date) date
                         (length time) time
-                        (length rst) rst
+                        (length rstrcvd) rstrcvd
+                        (length rstsent) rstsent
                         (length operator) operator
-                        (length location) location))
+                        (length location) location
+                        (length comment) comment))
         (forward-line))
 (write-file "~/radio-logbook.adi")))
