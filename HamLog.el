bf97ec187a87065ce39ad87d96c2a78447c41f33
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-04 06:28:56 -0800

    Update and rename Ham.lisp to HamLog.el

--- HamLog.el
+++ HamLog.el
@@ -1,3 +1,7 @@
+;; This code defines two functions: `add-log-entry` and `export-logbook-adif`. The `add-log-entry` function prompts the user for information about a new amateur radio contact and adds it to a text file called `radio-log.txt` in the user's home directory. The `export-logbook-adif` function reads the contents of `radio-log.txt` and converts it to ADIF format, writing the result to a file called `radio-logbook.adi`.
+
+;; To use these functions, you would first invoke `add-log-entry` to add one or more entries to the logbook, and then invoke `export-logbook-adif` to export the logbook to ADIF format. You can then use the resulting ADIF file to import the logbook into another logging program or platform that supports the ADIF format.
+
 ;; Define a function to add a new entry to the logbook
 (defun add-log-entry ()
   "Prompt the user for information about a new amateur radio contact and add it to the logbook."
@@ -42,8 +46,3 @@
                         (length location) location))
         (forward-line))
 (write-file "~/radio-logbook.adi")))
-
-
-;; This code defines two functions: `add-log-entry` and `export-logbook-adif`. The `add-log-entry` function prompts the user for information about a new amateur radio contact and adds it to a text file called `radio-log.txt` in the user's home directory. The `export-logbook-adif` function reads the contents of `radio-log.txt` and converts it to ADIF format, writing the result to a file called `radio-logbook.adi`.
-
-;; To use these functions, you would first invoke `add-log-entry` to add one or more entries to the logbook, and then invoke `export-logbook-adif` to export the logbook to ADIF format. You can then use the resulting ADIF file to import the logbook into another logging program or platform that supports the ADIF format.
