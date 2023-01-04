7d5c185aebca34b4146078c582278cbdda361f52
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-04 12:38:35 -0800

    Update and rename HamLog.el to QSO.el
    Consolidated two functions to one and automated date and time entry.

---
+++ QSO.el
@@ -0,0 +1,37 @@
+;; This code defines the function `add-qso` function which prompts the user for information about a new amateur radio contact and adds it to a text file called `radio-log.txt` in the user's home directory and to an ADIF file `radio-logbook.adi`.
+
+;; To use this functions, you would first invoke `add-qso` to add one or more entries to the logbook, and then import the resulting ADIF file to import the logbook into another logging program or platform that supports the ADIF format for further processing.
+
+(defun add-qso ()
+  "Prompt the user for information about a new amateur radio contact and add it to the logbook."
+  (let ((date (format-time-string "%Y%m%d" (current-time) t))
+        (time (format-time-string "%H%M%S" (current-time) t)))
+  (interactive)
+  (let ((callsign (read-string "Callsign: "))
+        (mode (read-string "Mode: "))
+        (frequency (read-string "Frequency: "))
+;;        (date (read-string "UTC Date (YYYYMMDD): "))
+;;        (time (read-string "UTC Time (HHMM): "))
+        (rstrcvd (read-string "RST Rcvd: "))
+        (rstsent (read-string "RST Sent: "))
+        (operator (read-string "Operator: "))
+        (location (read-string "Location: "))
+        (comment (read-string "Comment: ")))
+    (with-temp-buffer
+      (insert (format "Callsign: %s\nMode: %s\nFrequency: %s\nDate: %s\nTime: %s\nRST Rcvd: %s\nRST Sent: %s\nOperator: %s\nLocation: %s\nComment: %s\n"
+                      callsign mode frequency date time rstrcvd rstsent operator location comment))
+      (append-to-file (point-min) (point-max) "~/radio-log.txt")))
+    (with-temp-buffer    
+      (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_RCVD:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s<COMMENT:%d>%s\n"
+                        (length callsign) callsign
+                        (length mode) mode
+                        (length frequency) frequency
+                        (length date) date
+                        (length time) time
+                        (length rstrcvd) rstrcvd
+                        (length rstsent) rstsent
+                        (length operator) operator
+                        (length location) location
+                        (length comment) comment))
+        (forward-line))
+      (append-to-file (point-min) (point-max) "~/radio-logbook.adi")))
