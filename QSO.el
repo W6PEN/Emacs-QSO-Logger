8feefcba36a1b2e39be7e45fba0ad3211c2ca551
pentrago <56549055+pentrago@users.noreply.github.com>
2023-01-05 15:28:41 -0800

    Added an `export-adif` function

--- QSO.el
+++ QSO.el
@@ -1,6 +1,7 @@
-;; This code defines the functions to prompt the user for information about a new amateur radio contact (QSO) and adds it to a text file called `qso-log.txt` and an ADIF file `qso-log.adi` in the user's home directory.
+;; This code defines basic functions to capture and process new amateur radio contacts (QSOs). Two functions, each optimized to different use cases but still functional for most uses, capture and append new QSOs to a text file called `qso-log.txt` and an ADIF file `qso-log.adi` in the user's home directory. 
 ;; The `add-qso` function is optimized for use when responding to a CQ. The `add-qso-cq` function is optimized for use when calling CQ and expecting more than one response, so the mode and frequency are entered only once per session, and C-g exits the loop.
-;; The logbook(s) can then be processed within Emacs or import the ADIF file into another logging program or platform that supports the ADIF format for further processing.
+;; A third function, `export-adif`, is provided to allow for a wholesale export of the `qso-log.txt` to a different ADIF file. 
+;; The ADIF file can then be imported into another logging program or platform that supports the ADIF format for further processing or archiving.
 
 (defun add-qso ()
   "Prompt the user for information about a new amateur radio contact and add it to the logbook."
@@ -64,3 +65,35 @@
                         (length location) location
                         (length comment) comment))
                  (append-to-file (point-min) (point-max) "~/qso-log.adi")))))))
+
+;; Define a function to export the entire txt QSO logbook to a new file in ADIF format
+(defun export-adif ()
+  "Export the QSO logbook to a file in ADIF format."
+  (interactive)
+  (with-temp-buffer
+    (insert-file-contents "~/qso-log.txt")
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let ((callsign (progn (search-forward "Callsign: ") (thing-at-point 'word)))
+            (mode (progn (search-forward "Mode: ") (thing-at-point 'word)))
+            (frequency (progn (search-forward "Frequency: ") (thing-at-point 'word)))
+            (date (progn (search-forward "Date: ") (thing-at-point 'word)))
+            (time (progn (search-forward "Time: ") (thing-at-point 'word)))
+            (rstsent (progn (search-forward "RST Sent: ") (thing-at-point 'word)))
+            (rstrcvd (progn (search-forward "RST Rcvd: ") (thing-at-point 'word)))
+            (operator (progn (search-forward "Operator: ") (thing-at-point 'word)))
+            (location (progn (search-forward "Location: ") (thing-at-point 'word))))
+            (comment (progn (search-forward "Comment: ") (thing-at-point 'word))))
+        (insert (format "<CALL:%d>%s <MODE:%d>%s <FREQ:%d>%s <QSO_DATE:%d>%s <TIME_ON:%d>%s <RST_RCVD:%d>%s <RST_SENT:%d>%s <NAME:%d>%s <QTH:%d>%s<COMMENT:%d>%s\n"
+                        (length callsign) callsign
+                        (length mode) mode
+                        (length frequency) frequency
+                        (length date) date
+                        (length time) time
+                        (length rstrcvd) rstrcvd
+                        (length rstsent) rstsent
+                        (length operator) operator
+                        (length location) location
+                        (length comment) comment))
+        (forward-line))
+(write-file "~/qso-log-export.adi")))
