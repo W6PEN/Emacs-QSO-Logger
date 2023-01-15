77b8eee66ff508192a7ed2cd6111dc472d88353d
W6PEN <56549055+W6PEN@users.noreply.github.com>
2023-01-15 10:36:17 -0800

    time

--- qso.el
+++ qso.el
@@ -4,7 +4,7 @@
 
 ;; Author: W6PEN
 ;; Keywords: lisp
-;; Version: 0.6.2
+;; Version: 0.7.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
